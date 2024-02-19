31e382c28b8b8762863b92fa5b19775b11b5befb
Neo <c.min.tseng@gmail.com>
2024-02-19 15:52:31 +0800

    add crash report by XCrash

--- TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
+++ TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
@@ -1,10 +1,18 @@
 package org.telegram.messenger;
 
+import android.content.Context;
 import org.telegram.messenger.regular.BuildConfig;
+import org.telegram.messengerutils.XCrashHelper;
 
 public class ApplicationLoaderImpl extends ApplicationLoader {
     @Override
     protected String onGetApplicationId() {
         return BuildConfig.APPLICATION_ID;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        XCrashHelper.init(base);
     }
 }
